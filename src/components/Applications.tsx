837758832a42ff6a90dcef5cb917279731458c31
chapapau8u492u <chapapau8u492u@outlook.com>
2025-07-08 23:47:10 +0530

    updated /health

--- src/components/Applications.tsx
+++ src/components/Applications.tsx
@@ -88,7 +88,7 @@ export const Applications = () => {
 
   const checkBackendConnection = async () => {
     try {
-      const response = await fetch(`https://8ae62f2b-58a6-4dbd-854a-705f9d835a81-00-3jv2cfl5jmwc5.sisko.replit.dev/health`);
+      const response = await fetch(`https://job-hunter-backend-o2krd9i1k-zoros-projects-73717a6e.vercel.app/health`);
       setBackendConnected(response.ok);
     } catch (error) {
       setBackendConnected(false);
